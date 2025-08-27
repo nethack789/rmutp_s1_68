5b92bb5af0c9fe39bde0d447cbd5e262a583b0dc
Phunet kanthawong <nethack789@gmail.com>
2025-08-27 17:45:19 +0700

    uuu

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,8 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "../generate/client";
 
+const prisma = new PrismaClient();
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World!"));
@@ -8,6 +11,10 @@ app.get("/about", (c) => {
     return c.json ({
         message: "About Page"
     });
+});
+app.get("/profile", async (c) => {
+    const profile = await prisma.profile.findMany();
+    return c.json(profile);
 });
 
 export default app;
